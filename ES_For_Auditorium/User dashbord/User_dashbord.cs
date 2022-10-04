f2fd5ee2682e1029cb6bee93feac691eb7ea576a
wphkweerasinghe <hkalhara777@gmail.com>
2022-10-04 23:17:43 +0530

    Create dashbord without container forms

--- ES_For_Auditorium/User dashbord/User_dashbord.cs
+++ ES_For_Auditorium/User dashbord/User_dashbord.cs
@@ -12,9 +12,70 @@ namespace ES_For_Auditorium.User_dashbord
 {
     public partial class User_dashbord : Form
     {
+        //Filds
+        private Button currentButton;
+        private Form activeForm;
+        //constractor 
         public User_dashbord()
         {
             InitializeComponent();
+        }
+
+        private void panelContainer_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+        private void ActivateButton(Object btnSender)
+        {
+            if (btnSender == null)
+            {
+                if (currentButton !=(Button)btnSender)
+                {
+                    currentButton = (Button)btnSender;
+                    currentButton.ForeColor = Color.White;
+                }
+            }
+        }
+
+        private void btnEventCalender_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender);
+        }
+
+        private void btnReservation_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender);
+        }
+
+        private void btnRequestAudi_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender);
+        }
+
+        private void btnProfile_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender);
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void OpenChildForm(Form childForm, object btnSender)
+        {
+            if(activeForm != null)
+            { activeForm.Close(); }
+            ActivateButton(btnSender);
+            activeForm = childForm;
+            childForm.TopLevel = false;
+            childForm.FormBorderStyle = FormBorderStyle.None;
+            childForm.Dock = DockStyle.Fill;
+            this.panelContainer.Controls.Add(childForm);
+            this.panelContainer.Tag = childForm;
+            childForm.BringToFront();
+            childForm.Show();
+            lblTitle.Text = childForm.Text;
         }
     }
 }
