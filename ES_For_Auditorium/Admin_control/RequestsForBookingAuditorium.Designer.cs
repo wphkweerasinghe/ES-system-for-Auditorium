d515aae327220874298b167df937de3acee12e77
wphkweerasinghe <hkalhara777@gmail.com>
2022-10-13 18:34:24 +0530

    Show selected row's data from requests table

--- ES_For_Auditorium/Admin_control/RequestsForBookingAuditorium.Designer.cs
+++ ES_For_Auditorium/Admin_control/RequestsForBookingAuditorium.Designer.cs
@@ -28,34 +28,256 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.panelRequests = new System.Windows.Forms.Panel();
+            this.dgvEvent = new System.Windows.Forms.DataGridView();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblId = new System.Windows.Forms.Label();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.lblEname = new System.Windows.Forms.Label();
+            this.txtTime = new System.Windows.Forms.TextBox();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.txtDuration = new System.Windows.Forms.TextBox();
+            this.lblDuration = new System.Windows.Forms.Label();
+            this.lblDate = new System.Windows.Forms.Label();
+            this.txtPrice = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnAccept = new System.Windows.Forms.Button();
+            this.btnReject = new System.Windows.Forms.Button();
+            this.txtDate = new System.Windows.Forms.TextBox();
+            this.panelRequests.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEvent)).BeginInit();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // panelRequests
+            // 
+            this.panelRequests.Controls.Add(this.panel1);
+            this.panelRequests.Controls.Add(this.dgvEvent);
+            this.panelRequests.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelRequests.Location = new System.Drawing.Point(0, 0);
+            this.panelRequests.Name = "panelRequests";
+            this.panelRequests.Size = new System.Drawing.Size(1178, 662);
+            this.panelRequests.TabIndex = 0;
+            // 
+            // dgvEvent
+            // 
+            this.dgvEvent.AllowUserToAddRows = false;
+            this.dgvEvent.AllowUserToDeleteRows = false;
+            this.dgvEvent.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvEvent.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.DisplayedCellsExceptHeaders;
+            this.dgvEvent.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvEvent.Dock = System.Windows.Forms.DockStyle.Top;
+            this.dgvEvent.EditMode = System.Windows.Forms.DataGridViewEditMode.EditOnEnter;
+            this.dgvEvent.Location = new System.Drawing.Point(0, 0);
+            this.dgvEvent.Name = "dgvEvent";
+            this.dgvEvent.ReadOnly = true;
+            this.dgvEvent.RowHeadersWidth = 51;
+            this.dgvEvent.RowTemplate.Height = 24;
+            this.dgvEvent.Size = new System.Drawing.Size(1178, 259);
+            this.dgvEvent.TabIndex = 0;
+            this.dgvEvent.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.txtDate);
+            this.panel1.Controls.Add(this.btnReject);
+            this.panel1.Controls.Add(this.btnAccept);
+            this.panel1.Controls.Add(this.btnEdit);
+            this.panel1.Controls.Add(this.txtPrice);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.lblDate);
+            this.panel1.Controls.Add(this.txtDuration);
+            this.panel1.Controls.Add(this.lblDuration);
+            this.panel1.Controls.Add(this.txtTime);
+            this.panel1.Controls.Add(this.lblTime);
+            this.panel1.Controls.Add(this.txtName);
+            this.panel1.Controls.Add(this.lblEname);
+            this.panel1.Controls.Add(this.txtId);
+            this.panel1.Controls.Add(this.lblId);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 259);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1178, 403);
+            this.panel1.TabIndex = 1;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // lblId
+            // 
+            this.lblId.AutoSize = true;
+            this.lblId.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblId.Location = new System.Drawing.Point(55, 64);
+            this.lblId.Name = "lblId";
+            this.lblId.Size = new System.Drawing.Size(93, 20);
+            this.lblId.TabIndex = 0;
+            this.lblId.Text = "Request ID";
+            // 
+            // txtId
+            // 
+            this.txtId.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtId.Location = new System.Drawing.Point(184, 64);
+            this.txtId.Name = "txtId";
+            this.txtId.ReadOnly = true;
+            this.txtId.Size = new System.Drawing.Size(220, 27);
+            this.txtId.TabIndex = 1;
+            // 
+            // txtName
+            // 
+            this.txtName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtName.Location = new System.Drawing.Point(184, 149);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(220, 27);
+            this.txtName.TabIndex = 3;
+            // 
+            // lblEname
+            // 
+            this.lblEname.AutoSize = true;
+            this.lblEname.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEname.Location = new System.Drawing.Point(55, 149);
+            this.lblEname.Name = "lblEname";
+            this.lblEname.Size = new System.Drawing.Size(100, 20);
+            this.lblEname.TabIndex = 2;
+            this.lblEname.Text = "Event Name";
+            // 
+            // txtTime
+            // 
+            this.txtTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTime.Location = new System.Drawing.Point(184, 229);
+            this.txtTime.Name = "txtTime";
+            this.txtTime.Size = new System.Drawing.Size(220, 27);
+            this.txtTime.TabIndex = 5;
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTime.Location = new System.Drawing.Point(55, 229);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(87, 20);
+            this.lblTime.TabIndex = 4;
+            this.lblTime.Text = "Start Time";
+            // 
+            // txtDuration
+            // 
+            this.txtDuration.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDuration.Location = new System.Drawing.Point(184, 301);
+            this.txtDuration.Name = "txtDuration";
+            this.txtDuration.Size = new System.Drawing.Size(220, 27);
+            this.txtDuration.TabIndex = 7;
+            // 
+            // lblDuration
+            // 
+            this.lblDuration.AutoSize = true;
+            this.lblDuration.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDuration.Location = new System.Drawing.Point(55, 301);
+            this.lblDuration.Name = "lblDuration";
+            this.lblDuration.Size = new System.Drawing.Size(73, 20);
+            this.lblDuration.TabIndex = 6;
+            this.lblDuration.Text = "Duration";
+            // 
+            // lblDate
+            // 
+            this.lblDate.AutoSize = true;
+            this.lblDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDate.Location = new System.Drawing.Point(462, 67);
+            this.lblDate.Name = "lblDate";
+            this.lblDate.Size = new System.Drawing.Size(45, 20);
+            this.lblDate.TabIndex = 8;
+            this.lblDate.Text = "Date";
+            // 
+            // txtPrice
+            // 
+            this.txtPrice.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPrice.Location = new System.Drawing.Point(591, 131);
+            this.txtPrice.Name = "txtPrice";
+            this.txtPrice.Size = new System.Drawing.Size(220, 27);
+            this.txtPrice.TabIndex = 11;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(98, 151);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(462, 131);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(541, 39);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Requests for booking Auditorium";
+            this.label1.Size = new System.Drawing.Size(98, 20);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Ticket Price";
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEdit.Location = new System.Drawing.Point(920, 99);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(166, 39);
+            this.btnEdit.TabIndex = 12;
+            this.btnEdit.Text = "Edit Request";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // btnAccept
+            // 
+            this.btnAccept.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAccept.Location = new System.Drawing.Point(920, 242);
+            this.btnAccept.Name = "btnAccept";
+            this.btnAccept.Size = new System.Drawing.Size(166, 39);
+            this.btnAccept.TabIndex = 13;
+            this.btnAccept.Text = "Accept Request";
+            this.btnAccept.UseVisualStyleBackColor = true;
+            // 
+            // btnReject
+            // 
+            this.btnReject.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReject.Location = new System.Drawing.Point(645, 242);
+            this.btnReject.Name = "btnReject";
+            this.btnReject.Size = new System.Drawing.Size(166, 39);
+            this.btnReject.TabIndex = 14;
+            this.btnReject.Text = "Reject Request";
+            this.btnReject.UseVisualStyleBackColor = true;
+            // 
+            // txtDate
+            // 
+            this.txtDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDate.Location = new System.Drawing.Point(591, 60);
+            this.txtDate.Name = "txtDate";
+            this.txtDate.Size = new System.Drawing.Size(220, 27);
+            this.txtDate.TabIndex = 15;
             // 
             // RequestsForBookingAuditorium
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(1178, 662);
+            this.Controls.Add(this.panelRequests);
             this.Name = "RequestsForBookingAuditorium";
             this.Text = "RequestsForBookingAuditorium";
+            this.panelRequests.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEvent)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
+        private System.Windows.Forms.Panel panelRequests;
+        private System.Windows.Forms.DataGridView dgvEvent;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label lblEname;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.Label lblId;
+        private System.Windows.Forms.TextBox txtTime;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.TextBox txtDuration;
+        private System.Windows.Forms.Label lblDuration;
+        private System.Windows.Forms.Label lblDate;
+        private System.Windows.Forms.TextBox txtPrice;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnReject;
+        private System.Windows.Forms.Button btnAccept;
+        private System.Windows.Forms.TextBox txtDate;
     }
 }
