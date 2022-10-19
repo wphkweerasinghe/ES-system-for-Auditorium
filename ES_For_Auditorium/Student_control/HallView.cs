6dea2dedb777fa12ec909e0312094c313e92ee49
wphkweerasinghe <hkalhara777@gmail.com>
2022-10-19 20:02:54 +0530

    created hall seat view as HallView From

---
+++ ES_For_Auditorium/Student_control/HallView.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ES_For_Auditorium.Student_control
+{
+    public partial class HallView : Form
+    {
+        public HallView()
+        {
+            InitializeComponent();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
