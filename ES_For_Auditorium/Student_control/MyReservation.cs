e841d463e00214f2109084c4506aac98e8d0c2b4
wphkweerasinghe <hkalhara777@gmail.com>
2022-10-19 22:38:44 +0530

    Completed show my reservation form

--- ES_For_Auditorium/Student_control/MyReservation.cs
+++ ES_For_Auditorium/Student_control/MyReservation.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,19 @@ namespace ES_For_Auditorium.User_dashbord
         public MyReservation()
         {
             InitializeComponent();
+            ShowReservations();
+        }
+        public void ShowReservations()
+        {
+            //Show data from table
+            String conString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename='D:\Lectuers\2nd year\2nd semester\ES\Final assigement\AuditoriumReservationDB.mdf';Integrated Security=True;Connect Timeout=30";
+            String select = "SELECT reservation.id, event.name, reservation.seat_no FROM reservation, event WHERE reservation.event_id = event.id";
+
+            SqlDataAdapter adapter = new SqlDataAdapter(select, conString);
+            DataSet ds = new DataSet();
+
+            adapter.Fill(ds, "reservation_Details");
+            dgvReservations.DataSource = ds.Tables["reservation_Details"];
         }
     }
 }
